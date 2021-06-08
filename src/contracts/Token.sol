0b71608eb5320b0074dbc782554c45d8d5252ec2
pranavsawant22 <doomboy1212@gmail.com>
2021-06-09 01:03:03 +0530

    final

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,27 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
-
+  address public minter;
   //add minter changed event
-
-  constructor() public payable ERC20("Name", "Symbol") {
+  event MinterChanged(address indexed from, address to);
+  
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+  	require(msg.sender==minter, 'Error, only owner can change pass minter role');
+  	minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  } 
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error! msg.sender does not have minter role');
 		_mint(account, amount);
 	}
 }
